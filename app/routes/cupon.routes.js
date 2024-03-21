32c8c9bf42219f584a25dd252a6ab65cc3aff835
Roberto Pineda <rpinedaec83@gmail.com>
2024-03-21 14:53:29 -0500

    Verificacion de token

--- app/routes/cupon.routes.js
+++ app/routes/cupon.routes.js
@@ -1,6 +1,7 @@
 const controller = require("../controllers/cupon.controller");
+const { authJwt } = require("../middlewares");
 module.exports = (app)=>{
-    app.post('/api/cupon/add', controller.post);
+    app.post('/api/cupon/add', [authJwt.verifyToken, authJwt.isAdmin],controller.post);
     app.get('/api/cupon/',controller.getAll);
     app.put('/api/cupon/:id',controller.put);
     app.get('/api/cupon/:id',controller.getById);
